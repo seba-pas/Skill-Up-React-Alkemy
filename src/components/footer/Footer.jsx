dcb9dc84e07040bdfe7a70fcab915803804954c0
Kapee87 <kapserker@gmail.com>
2022-11-03 23:40:32 -0300

    empezando el footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+export default function Footer() {
+    return <footer>
+        <Box>
+            <h3>Footer</h3>
+        </Box>
+    </footer>;
+}
--- src/pages/Private/Home/Home.jsx
+++ src/pages/Private/Home/Home.jsx
@@ -7,6 +7,7 @@ import Saldo2 from '../../../components/saldo/Saldo2';
 import AvatarUser from '../../../components/user/AvatarUser';
 // styles
 import { ContentHome } from './Home.styles.';
+import Footer from '../../../components/footer/Footer';
 
 export default function Home() {
     // const [saldo, setSaldo] = useState(0);
@@ -23,12 +24,13 @@ export default function Home() {
             </div>
             <div className="circle2" />
 
-            <div>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <Buttons />
             </div>
             {/* <div style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
                 <Transacciones />
             </div> */}
+            <Footer />
         </ContentHome>
     );
 }
