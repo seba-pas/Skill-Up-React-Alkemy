b9c8344d22ff250d7b55fab68e7b8cc3da1bdf85
Kapee87 <kapserker@gmail.com>
2022-11-11 13:02:30 -0300

    fix a los links target_blank

--- src/pages/Private/Contact/Contact.jsx
+++ src/pages/Private/Contact/Contact.jsx
@@ -9,37 +9,44 @@ function Contact() {
             </Typography>
             <Container>
                 <Container>
-                    <Link href="https://github.com/DaikonCOde" id="alex">
+                    <Link
+                        href="https://github.com/DaikonCOde"
+                        id="alex"
+                        target="_blank"
+                        rel="noreferrer">
                         <Avatar src="https://avatars.githubusercontent.com/u/79052246?v=4" />
                     </Link>
                     <h2>Alex</h2>
                 </Container>
                 <Container>
-                    <Link href="https://github.com/Kapee87">
+                    <Link href="https://github.com/Kapee87" target="_blank" rel="noreferrer">
                         <Avatar src="https://avatars.githubusercontent.com/u/89433307?v=4" />
                     </Link>
                     <h2>Nahuel</h2>
                 </Container>
                 <Container>
-                    <Link href="https://github.com/seba-pas">
+                    <Link href="https://github.com/seba-pas" target="_blank" rel="noreferrer">
                         <Avatar src="https://avatars.githubusercontent.com/u/102265528?v=4" />
                     </Link>
                     <h2>Sebas</h2>
                 </Container>
                 <Container>
-                    <Link href="https://github.com/RodriguezDlaFlor">
+                    <Link
+                        href="https://github.com/RodriguezDlaFlor"
+                        target="_blank"
+                        rel="noreferrer">
                         <Avatar src="https://avatars.githubusercontent.com/u/116192201?v=4" />
                     </Link>
                     <h2>Flor</h2>
                 </Container>
                 <Container>
-                    <Link href="https://github.com/emifrese">
+                    <Link href="https://github.com/emifrese" target="_blank" rel="noreferrer">
                         <Avatar src="https://avatars.githubusercontent.com/u/80860297?v=4" />
                     </Link>
                     <h2>Emi</h2>
                 </Container>
                 <Container>
-                    <Link href="https://github.com/Nikl4auss">
+                    <Link href="https://github.com/Nikl4auss" target="_blank" rel="noreferrer">
                         <Avatar src="https://avatars.githubusercontent.com/u/65145528?v=4" />
                     </Link>
                     <h2>Niko</h2>
