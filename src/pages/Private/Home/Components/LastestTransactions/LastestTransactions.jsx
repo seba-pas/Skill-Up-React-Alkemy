2f1c5da5167858b3be88cd93f54ee909fe7c526e
Kapee87 <kapserker@gmail.com>
2022-11-11 18:23:36 -0300

    parcial home implement con api

--- src/pages/Private/Home/Components/LastestTransactions/LastestTransactions.jsx
+++ src/pages/Private/Home/Components/LastestTransactions/LastestTransactions.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 import { ContentLastestTransactions } from './LastestTransactions.styles';
 import { PRIVATE } from '../../../../../router/PathUrl';
 
 function LastestTransactions() {
+    const token = localStorage.getItem('token');
+    const [data, setData] = useState();
+    useEffect(() => {
+        axios
+            .get('http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com/transactions', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then((res) => {
+                setData(res.data.data);
+            })
+            .catch((err) => {
+                Swal.fire('', err, 'error');
+            });
+    }, []);
+    function isBill(value) {
+        if (value < 0) {
+            return 'red';
+        }
+        return 'black';
+    }
+
     return (
         <ContentLastestTransactions>
             <div className="top d-flex between">
@@ -12,17 +37,32 @@ function LastestTransactions() {
                     <h5 className="label">SEE ALL {'>'}</h5>
                 </Link>
             </div>
-            <div className="list-cards">
-                <div className="card d-flex between">
-                    <div className="article">
-                        <h4 className="text">Shopping</h4>
-                        <span className="date t-light">tue 25-10-2021</span>
-                    </div>
-                    <div className="amount">
-                        <h4 className="text"> $29.90</h4>
-                    </div>
-                </div>
-            </div>
+            {data
+                ? data.map((oneTransaction, index) => {
+                      if (index < 5) {
+                          return (
+                              <div className="list-cards" key={oneTransaction.id}>
+                                  <div className="card d-flex between">
+                                      <div className="article">
+                                          <h4 className="text">{oneTransaction.concept} </h4>
+                                          <span className="date t-light">
+                                              {oneTransaction.date}{' '}
+                                          </span>
+                                      </div>
+                                      <div className="amount">
+                                          <h4
+                                              className="text"
+                                              style={{ color: isBill(oneTransaction.amount) }}>
+                                              {oneTransaction.amount}{' '}
+                                          </h4>
+                                      </div>
+                                  </div>
+                              </div>
+                          );
+                      }
+                      return '';
+                  })
+                : ''}
         </ContentLastestTransactions>
     );
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
-import { userSlice } from './states/user';
+// import { userSlice } from './states/user';
 import { dataApi } from '../services/dataApi';
 
 export const store = configureStore({
     reducer: {
-        user: userSlice.reducer,
         [dataApi.reducerPath]: dataApi.reducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(dataApi.middleware)
